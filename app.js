ff4b0c979581744467b09eaf9892fb88ba5c5d9f
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-07-24 06:08:28 -0700

    method has been changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let arr = [{name: 'Dodge'},{name: 'Fodr Mustang'},{name: 'bmw'},{name: 'Ferrary'}];
+let arr = [{name: 'Dodge'},{name: 'Fodr Mustang'},{name: 'bmw'},{name: 'Porche'}];
 let secondArr = [];
 
 
-arr.forEach( function (value, index, array) {
+arr.map( function (value, index, array) {
     secondArr[index] = value.name;
 });
 
